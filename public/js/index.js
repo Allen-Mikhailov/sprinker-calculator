a3eb2e14078b7965047e11bc36ff9b278acdfce7
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2023-06-15 18:33:35 -0500

    Added dropddown and setup for easier rendering

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
-const screen = document.getElementById("screen")
+const simScreen = document.getElementById("sim-screen")
+const visibleMapSelector = document.getElementById("visible-map-selector")
 
 let gridWidth = 150
 let gridHeight = 100
@@ -12,13 +13,13 @@ function refreshScreen()
     const SCREEN_WIDTH = lineWidth*(gridWidth+1) + gridSize*gridWidth
     const SCREEN_HEIGHT = lineWidth*(gridHeight+1) + gridSize*gridHeight
 
-    screen.width = SCREEN_WIDTH
-    screen.height = SCREEN_HEIGHT+1
+    simScreen.width = SCREEN_WIDTH
+    simScreen.height = SCREEN_HEIGHT+1
 
-    screen.style.width = `${SCREEN_WIDTH}px`
-    screen.style.height = `${SCREEN_HEIGHT+1}px`
+    simScreen.style.width = `${SCREEN_WIDTH}px`
+    simScreen.style.height = `${SCREEN_HEIGHT+1}px`
 
-    const ctx = screen.getContext("2d");
+    const ctx = simScreen.getContext("2d");
 
     // Lines
     for (let i = 0; i <= gridWidth; i++)
@@ -33,18 +34,32 @@ function refreshScreen()
 
     const c = Math.hypot(gridWidth, gridHeight)
 
-    for (let x = 0; x < gridWidth; x++)
+    function applyToGrid(funct)
     {
-        for (let y = 0; y < gridHeight; y++)
-        {
+        for (let x = 0; x < gridWidth; x++)
+        {  
             let ax = lineWidth + x*gridSize + (x)*lineWidth
-            let ay = lineWidth + y*gridSize + (y)*lineWidth
-            const disAlpha = Math.hypot(x, y)/c
+            for (let y = 0; y < gridHeight; y++)
+            {
+                let ay = lineWidth + y*gridSize + (y)*lineWidth
 
-            ctx.fillStyle = `rgb(${(1-disAlpha)*255}, ${255}, ${(1-disAlpha)*255})`
-            ctx.fillRect(ax, ay, gridSize, gridSize)
+                funct(x, y, ax, ay)
+            }
         }
     }
+
+    applyToGrid((x, y, ax, ay) => {
+        const disAlpha = Math.hypot(x, y)/c
+
+        ctx.fillStyle = `rgb(${(1-disAlpha)*255}, ${255}, ${(1-disAlpha)*255})`
+        ctx.fillRect(ax, ay, gridSize, gridSize)
+    })
 }
 
+visibleMapSelector.onchange = refreshScreen
+
 refreshScreen()
+
+simScreen.onmousemove = (e) => {
+    //e.clientX
+}
